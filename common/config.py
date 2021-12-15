8c3caa188c5fcfcb8fbe9cb3a80f22a452d140f5
thangdc <thang@thangdc.com>
2021-12-15 11:01:17 +0700

    Not load train model if not exist

--- common/config.py
+++ common/config.py
@@ -3,11 +3,12 @@ import dlib
 import os,sys
 import pickle
 import json
+from os.path import exists as file_exists
 
 dirpath = os.path.realpath(os.path.dirname(sys.argv[0]))
 
-train_folder = os.path.join(dirpath, "train")
-train_folder = os.path.join(dirpath, "train", "thangdc.pkl")
+#train_folder = os.path.join(dirpath, "train")
+train_folder = os.path.join(dirpath, "train", "model.pkl")
 
 unknown_name = "Unknown"
 threshold_recognition = 5
@@ -17,5 +18,6 @@ predictor = dlib.shape_predictor(os.path.join(dirpath, 'models/shape_predictor_5
 facerec = dlib.face_recognition_model_v1(os.path.join(dirpath, 'models/dlib_face_recognition_resnet_model_v1.dat'))
 
 print('Loading {} model'.format(train_folder))
-with open(train_folder, 'rb') as f:
-    (le, clf) = pickle.load(f)
+if file_exists(train_folder):
+    with open(train_folder, 'rb') as f:
+        (le, clf) = pickle.load(f)
